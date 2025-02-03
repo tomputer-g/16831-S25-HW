6950bdc5b4a9478c346e2b59311708c7d11dde44
Tom Gao <zimingg@umich.edu>
2025-02-03 09:46:59 -0500

    Added print of avg/std for eval/train

--- hw1/run_all_bc.sh
+++ hw1/run_all_bc.sh
@@ -22,15 +22,45 @@ run_agent() {
         --num_agent_train_steps_per_iter 2000 \
         --video_log_freq -1 \
         --n_layers 2 2>/dev/null)    
+
+    #Example of output:
+    # Collecting data for eval...
+    # /home/tomg/src/16831-S25-HW/hw1/env/lib/python3.10/site-packages/gym/utils/passive_env_checker.py:241: DeprecationWarning: `np.bool8` is a deprecated alias for `np.bool_`.  (Deprecated NumPy 1.24)
+    #   if not isinstance(terminated, (bool, np.bool8)):
+    # Eval_AverageReturn : 341.92864990234375
+    # /home/tomg/src/16831-S25-HW/hw1/env/lib/python3.10/site-packages/tensorboardX/summary.py:153: DeprecationWarning: Conversion of an array with ndim > 0 to a scalar is deprecated, and will error in future. Ensure you extract a single element from your array before performing this operation. (Deprecated NumPy 1.25.)
+    #   scalar = float(scalar)
+    # Eval_StdReturn : 102.69835662841797
+    # Eval_MaxReturn : 569.781982421875
+    # Eval_MinReturn : 223.09657287597656
+    # Eval_AverageEpLen : 63.125
+    # Train_AverageReturn : 10344.517578125
+    # Train_StdReturn : 20.9814453125
+    # Train_MaxReturn : 10365.4990234375
+    # Train_MinReturn : 10323.5361328125
+    # Train_AverageEpLen : 1000.0
+    # Train_EnvstepsSoFar : 0
+    # TimeSinceStart : 2.883723020553589
+    # Training Loss : 0.013900777325034142
+    # Initial_DataCollection_AverageReturn : 10344.517578125
+    # Done logging...
     # Extract values
+    train_avg=$(echo "$output" | grep "Train_AverageReturn" | awk '{print $3}')
+    train_std=$(echo "$output" | grep "Train_StdReturn" | awk '{print $3}')
     eval_avg=$(echo "$output" | grep "Eval_AverageReturn" | awk '{print $3}')
-    initial_avg=$(echo "$output" | grep "Initial_DataCollection_AverageReturn" | awk '{print $3}')
+    eval_std=$(echo "$output" | grep "Eval_StdReturn" | awk '{print $3}')
+    # initial_avg=$(echo "$output" | grep "Initial_DataCollection_AverageReturn" | awk '{print $3}')
 
     # Calculate percentage
-    percentage=$(awk "BEGIN {printf \"%.3f\", ($eval_avg / $initial_avg) * 100}")
+    percentage=$(awk "BEGIN {printf \"%.3f\", ($eval_avg / $train_avg) * 100}")
     
     # Print agent name with result
-    echo "${agent}: ${percentage}%"
+    echo "${agent} eval/train: ${percentage}%"
+    # Print Eval avg/std
+    echo "Eval: ${eval_avg}/${eval_std}"
+    # Print Train avg/std
+    echo "Train: ${train_avg}/${train_std}"
+    echo "---------------------------------"
 }
 
 # Example usage with different agents
